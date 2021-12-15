fec880996da45f5b1daec9f553d741c806397744
Alocerio <learningtodie2day@gmail.com>
2021-12-15 17:51:25 -0300

    secondMailConfigUdate

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-import {ConfidentialClientApplication} from '@azure/msal-node';  
-
+import {ConfidentialClientApplication} from '@azure/msal-node'; 
 //import dotenv from 'dotenv';
-import fetch from 'node-fetch';
- 
-dotenv.config();
+//import fetch from 'node-fetch';
+//dotenv.config();
+
 //msal-node configuration
 const config = {
-    auth: { 
-        clientId: '13ce2cf3-92f5-4c1a-92a0-f4655a10d51b', //clientId: '13ce2cf3-92f5-4c1a-92a0-f4655a10d51b',
-        authority: 'https://login.microsoftonline.com/b3fc8718-0fcb-4f4b-9c95-c48db3c88e26', // Tenant-specific authority https://login.microsoftonline.com/b3fc8718-0fcb-4f4b-9c95-c48db3c88e26
-        clientSecret: 'Btx7Q~NEq6nUD7-4dy3MSk~XNfh2OUWlDHOCp', //Certificate ID = b3fc8718-0fcb-4f4b-9c95-c48db3c88e26
+    auth: {
+        clientId: "be2ed2d6-980a-49f1-9008-c408ae495753",
+        authority: "https://login.microsoftonline.com/msretailfederation.onmicrosoft.com",
+        clientCertificate: {
+            thumbprint: "734F07557964BEAA1F4786513E817BB3E92BEFBB", // a 40-digit hexadecimal string
+           privateKey: "46f3e425-b99a-49b8-bbea-1ead1e7c47a7",
+        }
     }
-}
-
-var client = new ConfidentialClientApplication(config);
+};
+//var client = new ConfidentialClientApplication(config);
+const cca =  new ConfidentialClientApplication(config);
 
 var request = { 
-    scopes: ['https://graph.microsoft.com/.default'],
+    scopes: ['https://sandbox.esd.channelinclusion.microsoft.com/'], // scopes for access token https://graph.microsoft.com/.default
 }
+//let response = await cca.acquireTokenByClientCredential(request);
+//console.dir(response);
+cca.acquireTokenByClientCredential(request).then((response) => {
+    console.log(response.accessToken);
+}).catch((error) => {
+    console.log(JSON.stringify(error));
+});
 
-let response = await client.acquireTokenByClientCredential(request);
 
-console.dir(response);
-
-
- let query = await fetch('https://graph.microsoft.com/v1.0/users ', {     //getUsers example
+/*
+ let query = await fetch('https://sandamericas.channelinclusiontest.microsoft.com', {     //getUsers example  https://graph.microsoft.com/v1.0/users
     headers: {
         'Authorization': `Bearer ${response.accessToken}`,
     },
 })  
 let json = await query.json();
-
-console.dir(json);
-
-
-
+//console.dir(json);
+*/
