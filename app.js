e6115e4fc84dffddc1db91ede8212beb89d2d304
Alocerio <learningtodie2day@gmail.com>
2021-12-16 22:14:19 -0300

    secondMailConfigUdate

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+https
+
+console.log('Server running at http://127.0.0.1:8081/');
+
 import {ConfidentialClientApplication} from '@azure/msal-node'; 
 //import dotenv from 'dotenv';
 //import fetch from 'node-fetch';
 //dotenv.config();
 
-//msal-node configuration
 const config = {
     auth: {
         clientId: "be2ed2d6-980a-49f1-9008-c408ae495753",
         authority: "https://login.microsoftonline.com/msretailfederation.onmicrosoft.com",
         clientCertificate: {
             thumbprint: "734F07557964BEAA1F4786513E817BB3E92BEFBB", // a 40-digit hexadecimal string
-            privateKey: "46f3e425-b99a-49b8-bbea-1ead1e7c47a7",
+            privateKey: "46f3e425-b99a-49b8-bbea-1ead1e7c47a7",  //46f3e425-b99a-49b8-bbea-1ead1e7c47a7 
         }
     }
 };
